a34b51e295effccb338f79a082813530adffa4f6
totomaro3 <ppoiuy145@gmail.com>
2023-03-08 15:58:07 +0900

    명령어 입력 테스트

---
+++ src/com/KoreaIT/java/AM/Main.java
@@ -0,0 +1,20 @@
+package com.KoreaIT.java.AM;
+
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println();
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String command1 = sc.nextLine();
+		System.out.printf("입력된 명령어 : %s\n", command1);
+		
+		int command2 = sc.nextInt();
+		System.out.printf("입력된 명령어 : %d\n", command2);
+
+	}
+
+}
