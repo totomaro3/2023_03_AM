330765dabc98ab36f1abf7d747d059a852e0af7d
totomaro3 <ppoiuy145@gmail.com>
2023-03-09 13:35:35 +0900

    게시물 목록

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -7,6 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		int lastArticleId = 0;
+		ArrayList<Article> articles = new ArrayList<Article>();
 		
 		System.out.println("==게시판 프로그램==");
 		
@@ -17,16 +18,23 @@ public class Main {
 			String command = sc.nextLine();
 			
 			if(command.equals("article list")) {
-				if(lastArticleId == 0) {System.out.print("게시글이 없습니다.\n");}
-				else System.out.printf("%d개의 글이 존재합니다.\n",lastArticleId);
+				if(articles.size() == 0) {System.out.print("게시글이 없습니다.\n");}
+				else {
+					System.out.println("번호\t/\t제목");
+					for(int i = 0; i<articles.size();i++) {
+						System.out.printf("%d\t/\t%s\n",articles.get(i).id,articles.get(i).title);
+					}
+				}
 			}
 			else if(command.equals("article write")) {
-				int id = ++lastArticleId;
+				
 				System.out.print("제목 : ");
 				String title = sc.nextLine();
 				System.out.print("내용 : ");
 				String content = sc.nextLine();
-				System.out.printf("%d번글이 생성되었습니다.\n",id);
+				Article article = new Article(title, content);
+				articles.add(article);
+				System.out.printf("%d번글이 생성되었습니다.\n",article.id);
 			}
 			else if(command.equals("exit")) {break;}
 			else if(command.equals("")) {System.out.print("명령어를 입력해주세요.\n");}
@@ -39,6 +47,27 @@ public class Main {
 
 	}
 
+}
+
+class Article {
+	static int lastArticleId = 0;
+	int id;
+	String title;
+	String content;
+	
+	static { //프로그램이 시작되면 시작
+		
+	}
+	
+	Article() {
+		
+	}
+	
+	Article(String title, String content) {
+		this.id = ++lastArticleId;
+		this.title = title;
+		this.content = content;
+	}
 }
 
 //git add .
